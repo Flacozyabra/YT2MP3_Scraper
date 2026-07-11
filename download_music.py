4c7410972d30a653edaf52f6256d34d4a766f808
Flacozyabra <falcon.flac@gmail.com>
2026-07-12 00:16:24 +0300

    feat: emulate Opera browser headers in ydl_opts

--- download_music.py
+++ download_music.py
@@ -101,6 +101,12 @@ def main():
             'youtube': {
                 'player_client': ['ios']
             }
+        },
+        # Emulate headers of Opera browser to match user's environment and TLS fingerprint
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         }
     }
     
