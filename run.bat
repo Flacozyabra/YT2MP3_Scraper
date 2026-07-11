bdcf4f6e6d89c4b2b7590de4640e82f7fbbe0804
Flacozyabra <falcon.flac@gmail.com>
2026-07-12 00:36:54 +0300

    fix: resolve duplicate parenthesis bug in run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 
 if not exist ".venv\Scripts\activate.bat" (
-    echo [ERROR] Python virtual environment (.venv) was not found in this folder.
+    echo [ERROR] Python virtual environment venv was not found in this folder.
     echo Please make sure the setup was completed successfully.
     pause
     exit /b 1
